cfe0a71a03c6ee869ca41d9903abd18644c95098
naillegal <quluzade.nail@gmail.com>
2024-03-06 01:06:48 +0400

    video-controls-added

--- js/main.js
+++ js/main.js
@@ -33,6 +33,21 @@ var swiper = new Swiper("#home-products .mySwiper", {
   },
 });
 
+// home-selected-projects
+var swiper = new Swiper("#home-selected-projects .mySwiper", {
+  slidesPerView: 1,
+  loop: true,
+  pagination: {
+    el: "#home-selected-projects .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: "#home-selected-projects .button-next",
+    prevEl: "#home-selected-projects .button-prev",
+  },
+});
+
+
 // home-featured-reels
 var swiper = new Swiper("#home-featured-reels .mySwiper", {
   loop: true,
@@ -48,170 +63,184 @@ var swiper = new Swiper("#home-featured-reels .mySwiper", {
   },
 });
 
-const videos = document.querySelectorAll('video');
-const playPauseBtns = document.querySelectorAll('.play-pause-btn');
+const videos = document.querySelectorAll("video");
+const playPauseBtns = document.querySelectorAll(".play-pause-btn");
 
-playPauseBtns.forEach(playPauseBtn => {
-  playPauseBtn.addEventListener('click', togglePlayPause);
+videos.forEach((video) => {
+  video.addEventListener("dblclick", toggleFullScreen);
+  video.addEventListener("play", handleVideoStateChange);
+  video.addEventListener("pause", handleVideoStateChange);
 });
 
-videos.forEach(video => {
-  video.addEventListener('play', handleVideoPlay);
-  video.addEventListener('pause', handleVideoPause);
+playPauseBtns.forEach((playPauseBtn) => {
+  playPauseBtn.addEventListener("click", togglePlayPause);
 });
 
-swiper.on('slideChange', handleSlideChange);
+swiper.on("slideChange", handleSlideChange);
 
-function togglePlayPause() {
-  const video = this.parentElement.querySelector('video');
-  const playIcon = this.querySelector('.fa-play');
-  const pauseIcon = this.querySelector('.fa-pause');
-
-  if (video.paused) {
-    video.play();
-    playIcon.style.display = 'none';
-    pauseIcon.style.display = 'inline-block';
+function toggleFullScreen() {
+  const video = this;
+  if (!document.fullscreenElement) {
+    if (video.requestFullscreen) {
+      video.requestFullscreen();
+    } else if (video.mozRequestFullScreen) {
+      /* Firefox */
+      video.mozRequestFullScreen();
+    } else if (video.webkitRequestFullscreen) {
+      /* Chrome, Safari ve Opera */
+      video.webkitRequestFullscreen();
+    } else if (video.msRequestFullscreen) {
+      /* Internet Explorer ve Edge */
+      video.msRequestFullscreen();
+    }
   } else {
-    video.pause();
-    playIcon.style.display = 'inline-block';
-    pauseIcon.style.display = 'none';
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      /* Firefox */
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      /* Chrome, Safari ve Opera */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      /* Internet Explorer ve Edge */
+      document.msExitFullscreen();
+    }
   }
 }
 
-function handleVideoPlay() {
-  playPauseBtns.forEach(playPauseBtn => {
-    const playIcon = playPauseBtn.querySelector('.fa-play');
-    const pauseIcon = playPauseBtn.querySelector('.fa-pause');
-
-    if (playPauseBtn.parentElement.querySelector('video') !== this) {
-      const otherVideo = playPauseBtn.parentElement.querySelector('video');
-      otherVideo.pause();
-      playIcon.style.display = 'inline-block';
-      pauseIcon.style.display = 'none';
-    }
-  });
+function togglePlayPause() {
+  const video = this.parentElement.querySelector("video");
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
 }
 
-function handleVideoPause() {
-  const playPauseBtn = this.parentElement.querySelector('.play-pause-btn');
-  const playIcon = playPauseBtn.querySelector('.fa-play');
-  const pauseIcon = playPauseBtn.querySelector('.fa-pause');
+function handleVideoStateChange() {
+  const playPauseBtn = this.parentElement.querySelector(".play-pause-btn");
+  const playIcon = playPauseBtn.querySelector(".fa-play");
+  const pauseIcon = playPauseBtn.querySelector(".fa-pause");
 
-  playIcon.style.display = 'inline-block';
-  pauseIcon.style.display = 'none';
+  if (this.paused) {
+    playIcon.style.display = "inline-block";
+    pauseIcon.style.display = "none";
+  } else {
+    playIcon.style.display = "none";
+    pauseIcon.style.display = "inline-block";
+  }
 }
 
 function handleSlideChange() {
-  videos.forEach(video => {
+  videos.forEach((video) => {
     video.pause();
-  });
-
-  playPauseBtns.forEach(playPauseBtn => {
-    const playIcon = playPauseBtn.querySelector('.fa-play');
-    const pauseIcon = playPauseBtn.querySelector('.fa-pause');
-    playIcon.style.display = 'inline-block';
-    pauseIcon.style.display = 'none';
+    const playPauseBtn = video.parentElement.querySelector(".play-pause-btn");
+    const playIcon = playPauseBtn.querySelector(".fa-play");
+    const pauseIcon = playPauseBtn.querySelector(".fa-pause");
+    playIcon.style.display = "inline-block";
+    pauseIcon.style.display = "none";
   });
 }
 
 // home-partners
-var swiper = new Swiper('#home-partners .mySwiper', {
+var swiper = new Swiper("#home-partners .mySwiper", {
   slidesPerView: 1,
   spaceBetween: 10,
   loop: true,
   pagination: {
-    el: '#home-partners .swiper-pagination',
+    el: "#home-partners .swiper-pagination",
     clickable: true,
   },
 });
 
-
 // home-statistics
 function animateCounters() {
-  let valueDisplays = document.querySelectorAll('#home-statistics .count');
+  let valueDisplays = document.querySelectorAll("#home-statistics .count");
   let interval = 1400;
 
   valueDisplays.forEach((valueDisplay) => {
-      let startValue = 0;
-      let endValue = parseInt(valueDisplay.getAttribute('data-val'));
-      let duration = Math.floor(interval / endValue);
-      let counter = setInterval(function () {
-          startValue += 1;
-          valueDisplay.textContent = startValue;
-          if (startValue == endValue) {
-              clearInterval(counter);
-          }
-      }, duration);
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+    let duration = Math.floor(interval / endValue);
+    let counter = setInterval(function () {
+      startValue += 1;
+      valueDisplay.textContent = startValue;
+      if (startValue == endValue) {
+        clearInterval(counter);
+      }
+    }, duration);
   });
 }
 
 function isCountersInViewport() {
-  let valueDisplays = document.querySelectorAll('#home-statistics .count');
+  let valueDisplays = document.querySelectorAll("#home-statistics .count");
   for (let valueDisplay of valueDisplays) {
-      let rect = valueDisplay.getBoundingClientRect();
-      if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-          return true; 
-      }
+    let rect = valueDisplay.getBoundingClientRect();
+    if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+      return true;
+    }
   }
-  return false; 
+  return false;
 }
 
-window.addEventListener('load', function() {
+window.addEventListener("load", function () {
   if (isCountersInViewport()) {
-      animateCounters();
+    animateCounters();
   }
 });
 
 let animationStarted = false;
 
-window.addEventListener('scroll', function() {
-  let currentScrollPos = window.pageYOffset || document.documentElement.scrollTop;
-  if (!animationStarted && isCountersInViewport() && currentScrollPos > lastScrollTop) {
-      animateCounters();
-      animationStarted = true; 
+window.addEventListener("scroll", function () {
+  let currentScrollPos =
+    window.pageYOffset || document.documentElement.scrollTop;
+  if (
+    !animationStarted &&
+    isCountersInViewport() &&
+    currentScrollPos > lastScrollTop
+  ) {
+    animateCounters();
+    animationStarted = true;
   }
   lastScrollTop = currentScrollPos <= 0 ? 0 : currentScrollPos;
 });
 
-window.addEventListener('DOMContentLoaded', function(event) {
+window.addEventListener("DOMContentLoaded", function (event) {
   animateCounters();
 });
 
 //layout.js
 
-const closeBtn = document.getElementById('menuClose');
-const menuBtn = document.getElementById('menuIcon');
-const menu = document.getElementById('menu');
-const bottom_overlay=document.getElementById('bottom-overlay')
-menuBtn.addEventListener('click', function () {
-    menu.classList.add('active')
-    bottom_overlay.classList.add('active')
+const closeBtn = document.getElementById("menuClose");
+const menuBtn = document.getElementById("menuIcon");
+const menu = document.getElementById("menu");
+const bottom_overlay = document.getElementById("bottom-overlay");
+menuBtn.addEventListener("click", function () {
+  menu.classList.add("active");
+  bottom_overlay.classList.add("active");
+});
+closeBtn.addEventListener("click", function () {
+  menu.classList.remove("active");
+  bottom_overlay.classList.remove("active");
+});
+function toggleList(listId) {
+  var list = document.getElementById(listId);
 
-  });
-  closeBtn.addEventListener('click', function () {
-    menu.classList.remove('active')
-    bottom_overlay.classList.remove('active')
-
-
-
-  });
-  function toggleList(listId) {
-    var list = document.getElementById(listId);
-
-    if (!list.classList.contains('open')) {
-        list.classList.add('open');
-        list.style.maxHeight = list.scrollHeight + 'px';
-    } else {
-        list.classList.remove('open');
-        list.style.maxHeight = '0';
-    }
+  if (!list.classList.contains("open")) {
+    list.classList.add("open");
+    list.style.maxHeight = list.scrollHeight + "px";
+  } else {
+    list.classList.remove("open");
+    list.style.maxHeight = "0";
+  }
 }
-window.addEventListener('scroll', function () {
-    var header = document.getElementById('header');
+window.addEventListener("scroll", function () {
+  var header = document.getElementById("header");
 
-    if (window.scrollY > 100) {
-      header.classList.add('scrolled');
-    } else {
-      header.classList.remove('scrolled');
-    }
-  });
+  if (window.scrollY > 100) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
